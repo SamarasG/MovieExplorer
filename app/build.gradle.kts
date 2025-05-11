a8b606561d69ae63ffe8533db9d00f0669fcebb0
Samaras Georgios <gsamaras@york.citycollege.eu>
2025-05-11 14:31:40 +0300

    123

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,9 +50,9 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
-    implementation(com.squareup.retrofit2:retrofit:2.9.0)
-    implementation(com.squareup.retrofit2:converter-gson:2.9.0)
-    implementation(com.squareup.okhttp3:logging-interceptor:4.9.3)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
